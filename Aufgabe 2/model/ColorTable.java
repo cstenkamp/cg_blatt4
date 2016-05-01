8bc9bd0a37eccac5305d79207afb7deea55da2c4
cebremer <cbremer@uos.de>
2016-05-01 21:58:59 +0200

    get Reduced

--- Aufgabe 2/model/ColorTable.java
+++ Aufgabe 2/model/ColorTable.java
@@ -4,8 +4,13 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class ColorTable {
+
+    private Color[] ReducedColors;
 	
 	public ColorTable(BufferedImage img, int numColors) {
+        ReducedColors = new Color[numColors];
+
+
 		// TODO: (A2) Bild analysieren und Ergebnis speichern
 		System.out.println("With: " + numColors);
 	}
@@ -17,15 +22,29 @@ public class ColorTable {
 	 * @return
 	 */
 	public Color getReducedColor(Color orig) {
-		// TODO: (A2) Auf die generierten Daten zugreifen, um eine
-		// passende Farbe zurück zu geben.
+		int index = 0;
+        float best = calcdist(orig, ReducedColors[0]);
+        for(int i = 1; i < ReducedColors.length; i++){
+            if(best > calcdist(orig, ReducedColors[i])){
+                best = calcdist(orig, ReducedColors[i]);
+                index = i;
+            }
+        }
+        return ReducedColors[index];
 		
 		// return dummy colors
-		if (orig.getGreen() > 100) {
-			return Color.WHITE;
-		} else {
-			return Color.BLUE;
-		}
+		//if (orig.getGreen() > 100) {
+		//	return Color.WHITE;
+		//} else {
+		//	return Color.BLUE;
+		//}
 	}
+
+    private float calcdist(Color colorA, Color colorB){
+        float red = Math.abs(colorA.getRed() - colorB.getRed());
+        float green = Math.abs(colorA.getGreen() - colorB.getGreen());
+        float blue = Math.abs(colorA.getBlue() - colorB.getBlue());
+        return (float) ((red+green+blue)/3.0);
+    }
 
 }
